3d6b18a6a40a470d5ee956dd9ee2b46b146cf409
Dari <dbarkov@stud.hs-heilbronn.de>
2025-05-14 11:30:27 +0200

    :)

--- src/main.go
+++ src/main.go
@@ -175,3 +175,62 @@ func main() {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
 }
+func main() {
+	// Serve static files (CSS, JS, etc.)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Handle routes
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/download", downloader)
+	http.HandleFunc("/query", query_downloader)
+
+	port := "8080"
+	fmt.Printf("Starting server on port %s...\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	// Fetch zettel list
+	zettels, err := get_zettel_list()
+	var zettelListHTML string
+	if err != nil {
+		zettelListHTML = "<p>Could not load zettel list.</p>"
+	} else {
+		zettelListHTML = "<ul>"
+		for _, z := range zettels {
+			// Make a link with the zettel name and referencing ZETTELSTORE_URL + "/h/" + z.Id
+			zettelListHTML += fmt.Sprintf("<li><a href=\"%s/h/%s\">%s</a></li>", ZETTELSTORE_URL, z.Id, z.Name)
+		}
+		zettelListHTML += "</ul>"
+	}
+
+	fmt.Fprintf(w, `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Welcome</title>
+            <link rel="stylesheet" href="/static/css/styles.css">
+        </head>
+        <body>
+            <nav class="zs-menu">
+                <a href="/">Home</a>
+                <a href="/download">Download ZIP</a>
+                <a href="/query?query=example">Query</a>
+            </nav>
+            <main>
+                <h1>Hello, World!</h1>
+                <p>Welcome to the server!</p>
+                <p><a href="/download">Download ZIP</a></p>
+                <p>Or you can <a href="/query?query=example">query a file</a>.</p>
+                <h2>Zettel List</h2>
+                %s
+            </main>
+        </body>
+        </html>
+    `, zettelListHTML)
+}
