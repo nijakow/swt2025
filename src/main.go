5ba62ba91f83761c02a83c87e0298c17ed6c153a
loktay <loktay@stud.hs-heilbronn.de>
2025-05-21 11:55:41 +0200

    In Dateien aufteilen

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 const ZETTELSTORE_URL = "http://localhost:23123"
+const OUR_PORT = "8080"
 
 // File struct represents a file with a name and content
 type File struct {
@@ -76,49 +77,6 @@ func get_zettel_list() ([]ZettelListEntry, error) {
 	return entries, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-
-	// Fetch zettel list
-	zettels, err := get_zettel_list()
-	var zettelListHTML string
-	if err != nil {
-		zettelListHTML = "<p>Could not load zettel list. Please start the Zettelstore.</p>"
-	} else {
-		zettelListHTML = "<ul>"
-		for _, z := range zettels {
-			// Make a link with the zettel name and referencing ZETTELSTORE_URL + "/h/" + z.Id
-			zettelListHTML += fmt.Sprintf("<li><a href=\"%s/h/%s\">%s</a></li>", ZETTELSTORE_URL, z.Id, z.Name)
-		}
-		zettelListHTML += "</ul>"
-	}
-
-	fmt.Fprintf(w, `
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Welcome</title>
-			<link rel="stylesheet" href="/static/css/styles.css">
-        </head>
-        <body>
-            <nav class="zs-menu">
-                <a href="/">Home</a>
-                <a href="/download">Download ZIP</a>
-                <a href="/query?query=example">Query</a>
-            </nav>
-            <main>
-                <h1>Hello, World!</h1>
-                <p>Welcome to the server!</p>
-                <p><a href="/download">Download ZIP</a></p>
-                <p>Or you can <a href="/query?query=example">query a file</a>.</p>
-                <h2>Zettel List</h2>
-                %s
-            </main>
-        </body>
-        </html>
-    `, zettelListHTML)
-}
-
 func downloader(w http.ResponseWriter, r *http.Request) {
 	// Create a buffer to write the ZIP file to
 	var buf bytes.Buffer
@@ -159,18 +117,6 @@ func downloader(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func query_downloader(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query().Get("query")
-	if query == "" {
-		http.Error(w, "Missing 'query' query parameter", http.StatusBadRequest)
-		return
-	}
-
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "You requested the file: %s\n", query)
-
-	// Request to the Zettelstore
-}
 
 func main() {
 	// Serve static files (CSS, JS, etc.)
@@ -181,7 +127,7 @@ func main() {
 	http.HandleFunc("/download", downloader)
 	http.HandleFunc("/query", query_downloader)
 
-	port := "8080"
+	port := OUR_PORT
 	fmt.Printf("Starting server on port %s...\n", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
